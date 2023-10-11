9dc9130306ee9874205b0d2e5203ceafe4fb2101
François-Nicolas Robinne <frobinne@psf.ca>
2023-10-11 14:03:24 -0600

    Code revisions to create graphs (Fig.3 in data descriptor) with catchment area in log10 scale.

--- 03_ComputeStatistics_Graphics.R
+++ 03_ComputeStatistics_Graphics.R
@@ -31,14 +31,18 @@ options(scipen = 6, digits = 4) # non-scientific notation
 library(sf)
 library(tidyverse)
 library(ggpubr)
+library(ggplot2)
+library(dplyr)
+library(scales)
+library(ggtext)
 
 ## Set working directory -------------------------------------------------------
 
-setwd("C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/02_PROCESSED_DATA")
+setwd("C:/Users/frobinne/Documents/PROFESSIONAL/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/")
 
 ## Processing ------------------------------------------------------------------
 
-canswap <- st_read("NRCAN/Unnest_Basins/Municipal_Catchments_V1/Can_SWaP_AllLicences_V2_CleanAtt.gpkg")
+canswap <- st_read("03_ANALYSIS_RESULTS/CAN-SWaP/Unnest_Basins/Municipal_Catchments_V1/Can_SWaP_AllLicences_V2_CleanAtt.gpkg")
   
   # Area statistics
   print(median(canswap$AREA * 0.000001)) #In square kilometers
@@ -65,83 +69,122 @@ canswap <- st_read("NRCAN/Unnest_Basins/Municipal_Catchments_V1/Can_SWaP_AllLice
   print(range(canswap$HFT_IX_U09))
   
   # Boxplots
+  # COnversion and binning of area
+  canswap <- canswap %>%
+    mutate(AREA_l10 = log10(AREA)) %>% # Conversion to log scale
+    mutate(BINS_CUT = cut(AREA_l10, # Cut in 7 quantiles
+                          breaks = c(6,7,8,9,10,11,12,13),
+                          include.lowest = F,
+                          right = F))
+  
   # Forest cover
-  bxp_for <- ggboxplot(canswap, x = "ORD_STRA", y = "FOR_PC_USE",
+  bxp_for <- ggboxplot(canswap, x = "BINS_CUT", y = "FOR_PC_USE",
                        color = "forestgreen", fill = "yellowgreen", add = "jitter",
                        add.params = list(size = 0.5, jitter = 0.3),
-                       xlab = "Strahler order",
                        size = 0.7, width = 0.8) +
     theme_grey() +
     ggtitle("Forest cover (%)") +
     theme(axis.line = element_line(linewidth = 1, colour = "gray30"),
           panel.border = element_rect(color = "gray30", fill = NA, linewidth = 1),
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30")) +
-    font("xy.text", size = 12, face = "bold", color = "gray30") +
-    font("xylab", size = 12, face = "bold", color = "gray30") +
+          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30"),
+          axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.7)) +
+    font("y.text", size = 9, face = "bold", color = "gray30") +
+    font("x.text", size = 9, face = "bold", color = "gray30") +
     rremove("legend") +
-    rremove("ylab")
+    rremove("ylab") +
+    rremove("xlab") +
+    scale_x_discrete(labels=c(expression(bold(paste("10"^"6", "-10"^"7"))), 
+                              expression(bold(paste("10"^"7", "-10"^"8"))),
+                              expression(bold(paste("10"^"8", "-10"^"9"))),
+                              expression(bold(paste("10"^"9", "-10"^"10"))),
+                              expression(bold(paste("10"^"10", "-10"^"11"))),
+                              expression(bold(paste("10"^"11", "-10"^"12"))),
+                              expression(bold(paste("10"^"12", "-10"^"13")))))
       
   #bxp_for # Call to visualize the plot if need be
-  
+
   # Wetland
-  bxp_wet <- ggboxplot(canswap, x = "ORD_STRA", y = "WET_PC_UG2",
+  bxp_wet <- ggboxplot(canswap, x = "BINS_CUT", y = "WET_PC_UG2",
                        color = "darkcyan", fill = "cyan3", add = "jitter",
                        add.params = list(size = 0.5, jitter = 0.3),
-                       xlab = "Strahler order",
                        size = 0.7, width = 0.8) +
     theme_grey() +
     ggtitle("Wetland cover (%)") +
     theme(axis.line = element_line(linewidth = 1, colour = "gray30"),
           panel.border = element_rect(color = "gray30", fill = NA, linewidth = 1),
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30")) +
-    font("xy.text", size = 12, face = "bold", color = "gray30") +
-    font("xylab", size = 12, face = "bold", color = "gray30") +
+          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30"),
+          axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.7)) +
+    font("xy.text", size = 9, face = "bold", color = "gray30") +
+    font("xylab", size = 9, face = "bold", color = "gray30") +
     rremove("legend") +
-    rremove("ylab")
+    rremove("ylab") +
+    rremove("xlab") +
+    scale_x_discrete(labels=c(expression(bold(paste("10"^"6", "-10"^"7"))), 
+                              expression(bold(paste("10"^"7", "-10"^"8"))),
+                              expression(bold(paste("10"^"8", "-10"^"9"))),
+                              expression(bold(paste("10"^"9", "-10"^"10"))),
+                              expression(bold(paste("10"^"10", "-10"^"11"))),
+                              expression(bold(paste("10"^"11", "-10"^"12"))),
+                              expression(bold(paste("10"^"12", "-10"^"13")))))
   
   bxp_wet_2 <- ggpar(bxp_wet, ylim = c(0,100)) # Adjust the y axis to be 0-100
-  
   #bxp_wet_2 # Call to visualize the plot if need be
   
   # Protected area
-  bxp_pac <- ggboxplot(canswap, x = "ORD_STRA", y = "PAC_PC_USE",
+  bxp_pac <- ggboxplot(canswap, x = "BINS_CUT", y = "PAC_PC_USE",
                        color = "gold4", fill = "gold", add = "jitter",
                        add.params = list(size = 0.5, jitter = 0.3),
-                       xlab = "Strahler order",
                        size = 0.7, width = 0.8) +
     theme_grey() +
     ggtitle("Protected area (%)") +
     theme(axis.line = element_line(linewidth = 1, colour = "gray30"),
           panel.border = element_rect(color = "gray30", fill = NA, linewidth = 1),
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30")) +
-    font("xy.text", size = 12, face = "bold", color = "gray30") +
-    font("xylab", size = 12, face = "bold", color = "gray30") +
+          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30"),
+          axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.7)) +
+    font("xy.text", size = 9, face = "bold", color = "gray30") +
+    font("xylab", size = 9, face = "bold", color = "gray30") +
     rremove("legend") +
-    rremove("ylab")
+    rremove("ylab")+
+    rremove("xlab") +
+    scale_x_discrete(labels=c(expression(bold(paste("10"^"6", "-10"^"7"))), 
+                              expression(bold(paste("10"^"7", "-10"^"8"))),
+                              expression(bold(paste("10"^"8", "-10"^"9"))),
+                              expression(bold(paste("10"^"9", "-10"^"10"))),
+                              expression(bold(paste("10"^"10", "-10"^"11"))),
+                              expression(bold(paste("10"^"11", "-10"^"12"))),
+                              expression(bold(paste("10"^"12", "-10"^"13")))))
   
   #bxp_pac # Call to visualize the plot if need be
   
   # Human footprint
-  bxp_hft <- ggboxplot(canswap, x = "ORD_STRA", y = "HFT_IX_U09",
+  bxp_hft <- ggboxplot(canswap, x = "BINS_CUT", y = "HFT_IX_U09",
                        color = "chocolate4", fill = "chocolate", add = "jitter",
                        add.params = list(size = 0.5, jitter = 0.3),
-                       xlab = "Strahler order",
                        size = 0.7, width = 0.8) +
     theme_grey() +
     ggtitle("Human Footprint Index (unitless)") +
     theme(axis.line = element_line(linewidth = 1, colour = "gray30"),
           panel.border = element_rect(color = "gray30", fill = NA, linewidth = 1),
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30")) +
-    font("xy.text", size = 12, face = "bold", color = "gray30") +
-    font("xylab", size = 12, face = "bold", color = "gray30") +
+          plot.title = element_text(hjust = 0.5, size = 14, face = "bold", color = "gray30"),
+          axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.7)) +
+    font("xy.text", size = 9, face = "bold", color = "gray30") +
+    font("xylab", size = 9, face = "bold", color = "gray30") +
     rremove("legend") +
-    rremove("ylab")
+    rremove("ylab")+
+    rremove("xlab") +
+    scale_x_discrete(labels=c(expression(bold(paste("10"^"6", "-10"^"7"))), 
+                              expression(bold(paste("10"^"7", "-10"^"8"))),
+                              expression(bold(paste("10"^"8", "-10"^"9"))),
+                              expression(bold(paste("10"^"9", "-10"^"10"))),
+                              expression(bold(paste("10"^"10", "-10"^"11"))),
+                              expression(bold(paste("10"^"11", "-10"^"12"))),
+                              expression(bold(paste("10"^"12", "-10"^"13")))))
   
-  #bxp_hft # Call to visualize the plot if need be
+  bxp_hft # Call to visualize the plot if need be
   
   # Arrange plots and export to png 
   # Figure 3
-  png(file = "C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/04_DOCS/Fig3_Boxplots.png",
-      width = 600, height = 450)
+  png(file = "C:/Users/frobinne/Documents/PROFESSIONAL/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/04_DOCS/Fig3_Boxplots.png",
+      units = 'cm', width = 19, height = 15, res = 350)
   ggarrange(bxp_for, bxp_wet_2, bxp_pac, bxp_hft)
   dev.off()
