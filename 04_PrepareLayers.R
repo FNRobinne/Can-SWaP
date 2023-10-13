dd843f5bcbc137a15613d8168b78d6b335ba76f2
François-Nicolas Robinne <frobinne@psf.ca>
2023-10-13 08:48:34 -0600

    Code updates for revised version of Can-SWaP (removed NoISC version for now)

--- 04_PrepareLayers.R
+++ 04_PrepareLayers.R
@@ -42,7 +42,6 @@ setwd("C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOUR
   Intakes_NoISC <- st_read("HYDROLAB_MCGILL/Intakes_RiverAtlas_Segments_With_Licence_V5_NoISC.gpkg")
   Confluences <- st_read("HYDROLAB_MCGILL/Intakes_RiverAtlas_Segments_With_Licence_Confluence_V1.gpkg")
   CanSwap <- st_read("NRCAN/Unnest_Basins/Municipal_Catchments_V1/Can_SWaP_AllLicences_V2_CleanAtt.gpkg")
-  CanSwap_NoISC <- st_read("NRCAN/Unnest_Basins/Municipal_Catchments_V1/Can_SWaP_NoISCLicences_V2_CleanAtt.gpkg")
 
   # Clean-Up data and attributes for versions to be published
   # All intakes
@@ -61,18 +60,6 @@ setwd("C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOUR
   Intakes_WGS84_NSD <- Intakes_NSD %>%
     st_transform(crs = 'EPSG:4326')
   
-  # No ISC intakes (only licenced ones)
-  Intakes_NoISC_NSD <- Intakes_NoISC %>%
-    select(-c(angle, distance, vertex_pos, vertex_index, vertex_part, vertex_part_index, YR_VOL_M3_count)) %>%
-    mutate(DIS_M3_TYR = dis_m3_pyr * 31540000) %>% # total per year, based on discharge on M3 per second
-    select(-dis_m3_pyr) %>%
-    rename(WET_PC_UG2 = wet_pc_ug2,
-           FOR_PC_USE = for_pc_use,
-           PAC_PC_USE = pac_pc_use,
-           HFT_IX_U09 = hft_ix_u09,
-           INT_M3_YR = YR_VOL_M3_sum,
-           JUR_NAME = PRENAME)
-  
   # No ISC intakes (only licenced ones) in WGS84
   Intakes_WGS84_NoISC_NSD <- Intakes_NoISC_NSD %>%
     st_transform(crs = 'EPSG:4326')
@@ -86,11 +73,6 @@ setwd("C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOUR
   
   # Can-SWaP polygons (i.e., Catchments)
   CanSwap_NSD <- CanSwap %>%
-    select(-YR_VOL_M3_count) %>%
-    relocate(DIS_M3_TYR, .before = INT_M3_YR)
-  
-  # Can-SWaP polygons (i.e., Catchments), No ISC (only licenced ones)
-  CanSwap_NoISC_NSD <- CanSwap_NoISC %>%
     select(-YR_VOL_M3_count) %>%
     relocate(DIS_M3_TYR, .before = INT_M3_YR)
 
@@ -114,5 +96,5 @@ setwd("C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOUR
            "C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/04_DOCS/SUBMISSION/DATASETS_FIGSHARE/can-SWaP_AllLicences.gpkg",
            delete_dsn = T)
   st_write(CanSwap_NSD,
-           "C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/04_DOCS/SUBMISSION/DATASETS_FIGSHARE/can-SWaP_NoISCLicences.gpkg",
+           "C:/Users/frobinne/Documents/Professional/PROJECTS/39_2021_CANADA_F2F_SOURCE2TAP_ACTIVE/04_DOCS/SUBMISSION/DATASETS_FIGSHARE/can-SWaP_AllLicences.csv",
            delete_dsn = T)
